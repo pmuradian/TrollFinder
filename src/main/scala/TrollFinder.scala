7f6ce1adcc9a07e54c2f2a7bea94a9e1596816b0
pmuradian <p.muradian@gmail.com>
2019-06-02 16:28:05 +0200

    Filtera and print spammers

--- src/main/scala/TrollFinder.scala
+++ src/main/scala/TrollFinder.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.util.Random
@@ -40,10 +41,16 @@ class TrollFinder {
 
   val arr = List[Integer]()
 
+  var arbabik = Seq.fill(5000)(Random.nextInt(100))
   var bikct = Seq.fill(5000)(Random.nextInt(1000))
-  var bvpuct = Seq.fill(5000)(Random.nextInt(10))
+  var bvpuct = Seq.fill(5000)(Random.nextInt(9) + 1)
+  arbabik = arbabik.union(Seq.fill(250)(Random.nextInt(100) + 100))
+  bikct = bikct.union(Seq.fill(250)(Random.nextInt(1000)))
+  bvpuct = bvpuct.union(Seq.fill(170)(1))
+  bvpuct = bvpuct.union(Seq.fill(80)(10))
 
-  val items = sc.parallelize(bikct zip bvpuct)//textFile(hdfsPath ++ "/user/azazel/*.csv").cache()
+  val ratings = sc.parallelize(arbabik zip (bikct zip bvpuct)).cache()//textFile(hdfsPath ++ "/user/azazel/*.csv").cache()
+//  val userID_ratings = sc.parallelize(arbabik zip items)
 
 //  items.foreach(x => println(x.toString))
 
@@ -53,9 +60,24 @@ class TrollFinder {
 
 //  movieID_rating.foreach( x => println(x))
 
-  val reduced = items.groupByKey()
+  val ratingTuples = ratings.map(x => (x._2._1, x._2._2))
+  val userID_ratings = ratings.map(x => (x._1, x._2._2))
+
+  val movieID_median = ratingTuples.groupByKey()
                       .map(x => (x._1, x._2.toList.sortWith(_ < _)))
                       .map(x => (x._1, Math.median(x._2)))
+
+//  val userID_movieID_median = ratings.map(x => (x._2._1, x._1)).join(movieID_median).cache()
 //
-  reduced.foreach(x => println(x).toString)
+//  userID_movieID_median.map(x => x)
+//
+  val potential_spammers = userID_ratings.groupByKey()
+                .map(x => (x._1, x._2.size, x._2.toList.sum))
+                .filter(x => (x._2 == x._3) || (10 * x._2 == x._3))
+                .map(x => (x._1, 0))
+
+  val collected = potential_spammers.join(ratings).map(x => (x._2._2._1, (x._1, x._2._2._2))).join(movieID_median)
+  val spammers = collected.filter(x => x._2._2 - x._2._1._2 >= 5 || x._2._2 - x._2._1._2 <= -5).map(x => x._2._1._1).distinct()
+  println("---------- Found spammers -----------")
+  spammers.foreach(println)
 }
